b35a6c865862262af1c6f67bc71d32b77a853dcb
piersvelander-collab <pier.svelander@gmail.com>
2026-04-21 20:20:06 +0200

    Update app.py

--- el_bot_projekt/app.py
+++ el_bot_projekt/app.py
@@ -6,7 +6,7 @@ from langchain_community.document_loaders import DirectoryLoader, TextLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain_google_genai import ChatGoogleGenerativeAI, GoogleGenerativeAIEmbeddings
-from langchain.chains.retrieval import create_retrieval_chain
+from langchain.chains import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
 
