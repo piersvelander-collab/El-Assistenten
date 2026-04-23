896d8054fde8086b12cade17a2a0f5465ff60a35
piersvelander-collab <pier.svelander@gmail.com>
2026-04-23 16:26:28 +0200

    Update app.py

--- el_bot_projekt/app.py
+++ el_bot_projekt/app.py
@@ -39,7 +39,7 @@ def load_knowledge_base():
 @st.cache_resource(show_spinner=False)
 def get_chat_model():
     # BYTT TILL "PRO" FÖR ATT SLIPPA ÖVERBELASTNING. STREAMING AKTIVERAT.
-    return ChatGoogleGenerativeAI(model="gemini-1.5-pro", temperature=0.0, max_retries=5, streaming=True)
+    return ChatGoogleGenerativeAI(model="gemini-2.5-pro", temperature=0.0, max_retries=5, streaming=True)
 
 vectorstore = load_knowledge_base()
 chat_model = get_chat_model()
