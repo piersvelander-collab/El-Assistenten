b902794f20ed748d77edc3d0678189b50bba84bd
piersvelander-collab <pier.svelander@gmail.com>
2026-04-25 09:54:57 +0200

    Update app.py

--- el_bot_projekt/app.py
+++ el_bot_projekt/app.py
@@ -86,7 +86,13 @@ def get_google_sheet(sheet_name):
         if "gcp_service_account" in st.secrets:
             import gspread
             from google.oauth2.service_account import Credentials
-            scopes = ["https://www.googleapis.com/auth/spreadsheets"]
+            
+            # HÄR ÄR FIXEN: Vi lägger till Google Drive-behörigheten!
+            scopes = [
+                "https://www.googleapis.com/auth/spreadsheets",
+                "https://www.googleapis.com/auth/drive"
+            ]
+            
             creds = Credentials.from_service_account_info(st.secrets["gcp_service_account"], scopes=scopes)
             client = gspread.authorize(creds)
             return client.open("El-Assistenten Logg").worksheet(sheet_name)
